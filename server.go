75c61afa82cd7c6ec23fc80ca829e652f8490f8b
Greaux <22226093+Greaux@users.noreply.github.com>
2023-10-25 12:14:28 +0300

    v0.2 pre-alpha
    1) Добавил функцию изменения данных (по id)
    2) Добавил функцию получения данных с фильтрами, лимитом и страницами (пагинация)
    3) добавил функцию удаления данных (по id)

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -28,6 +29,22 @@ type App struct {
 }
 
 func (app *App) Initialize() {
+	/*
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+			godotenv.Load("DB_HOST"),
+			godotenv.Load("DB_USER"),
+			godotenv.Load("DB_PASSWORD"),
+			godotenv.Load("DB_NAME"),
+			godotenv.Load("DB_PORT"),
+			godotenv.Load("DB_SSL_MODE"),
+		)
+	*/
+
 	dsn := "host=db user=postgres password=XDDDPASSW0RD dbname=EFMOBILPersons port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -169,6 +186,149 @@ func (app *App) AddPerson(c *fiber.Ctx) error {
 
 }
 
+func (app *App) DeleteData(c *fiber.Ctx) error {
+	uid := c.FormValue("id")
+	var person Person
+
+	if err := app.DB.Where("id = ?", uid).Delete(&person).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	app.DB.Delete(&person)
+	return c.JSON(fiber.Map{
+		"message": "Person deleted successfully",
+	})
+}
+
+func (app *App) UpdateData(c *fiber.Ctx) error {
+	id := c.FormValue("id")
+	name := c.FormValue("name")
+	surname := c.FormValue("surname")
+	patronymic := c.FormValue("patronymic")
+	gender := c.FormValue("gender")
+	age := c.FormValue("age")
+	nationality := c.FormValue("nationality")
+
+	// Находим запись в базе данных по ID
+	var person Person
+	if err := app.DB.First(&person, id).Error; err != nil {
+		return err
+	}
+
+	if name != "" {
+		person.Name = name
+	}
+
+	if surname != "" {
+		person.Surname = surname
+	}
+
+	if patronymic != "" {
+		person.Patronymic = patronymic
+	}
+
+	if gender != "" {
+		person.Gender = gender
+
+	}
+
+	if age != "" {
+		// Преобразуем возраст в число
+		ageInt, err := strconv.Atoi(age)
+		if err != nil {
+			return err
+		}
+		person.Age = ageInt
+	}
+
+	if nationality != "" {
+		person.Nationality = nationality
+	}
+
+	// Сохраняем изменения в базе данных
+	if err := app.DB.Save(&person).Error; err != nil {
+		return err
+	}
+
+	// Возвращаем успешный ответ
+	return c.SendString("Данные успешно изменены")
+}
+
+func (app *App) GetData(c *fiber.Ctx) error {
+	// Получаем значения из запроса
+	name := c.FormValue("name")
+	surname := c.FormValue("surname")
+	patronymic := c.FormValue("patronymic")
+	gender := c.FormValue("gender")
+	age := c.FormValue("age")
+	nationality := c.FormValue("nationality")
+
+	limit := c.FormValue("limit")
+	page := c.FormValue("page")
+
+	if limit == "" {
+		limit = "10"
+	}
+
+	if page == "" {
+		page = "1"
+	}
+
+	limitInt, err := strconv.Atoi(limit)
+	if err != nil {
+		// Обработка ошибки преобразования limit
+		return err
+	}
+	pageInt, err := strconv.Atoi(page)
+	if err != nil {
+		// Обработка ошибки преобразования page
+		return err
+	}
+
+	// Вычисляем смещение (offset) на основе значения page и limit
+	offset := (pageInt - 1) * limitInt
+
+	var results []Person
+
+	query := app.DB
+	query = query.Offset(offset).Limit(limitInt)
+
+	if name != "" {
+		query = query.Where("name = ?", name)
+	}
+	if patronymic != "" {
+		query = query.Where("patronymic =?", patronymic)
+	}
+
+	if gender != "" {
+		query = query.Where("gender =?", gender)
+	}
+
+	if nationality != "" {
+		query = query.Where("nationality =?", nationality)
+	}
+
+	if surname != "" {
+		query = query.Where("surname =?", surname)
+	}
+
+	if age != "" {
+		ageInt, err := strconv.Atoi(age)
+		if err != nil {
+			return err
+		}
+		query = query.Where("age = ?", ageInt)
+	}
+
+	if err := query.Find(&results).Error; err != nil {
+		return err
+	}
+
+	return c.JSON(results)
+}
+
 func main() {
 	app := &App{}
 
@@ -177,10 +337,20 @@ func main() {
 
 	appFiber := fiber.New()
 
-	// appFiber.Get("/database/:Name", app.GetData)
+	//в Процессе
+
+	// Сделано
+	appFiber.Get("/database", app.GetData)
+	appFiber.Post("/database/edit", app.UpdateData)
 	appFiber.Get("/", app.MainPage)
-	// appFiber.Delete("/database/:uID", app.DeleteData)
+	appFiber.Delete("/database", app.DeleteData)
 	appFiber.Post("/database", app.AddPerson)
 
 	log.Fatal(appFiber.Listen(":3000"))
 }
+
+//// TODO: |||
+//// env   |||
+//// logs  |||
+//// docs  |||
+//// comms |||
